4b31b903eb49cbfb07f189aedb1fa02639cf6e73
Wpl0o0lqW <idixof@gmail.com>
2025-08-31 23:47:35 +0900

    Update .lua

--- .lua
+++ .lua
@@ -5,6 +5,7 @@ local gmids = {
     121864768012064, -- fish it
     88116343972246, -- sleepy brairot
     98784084213911, -- weird gun game
+    82097489006022
 }
 
 local id = game.PlaceId
@@ -15,7 +16,7 @@ if id == gmids[1] then
 elseif id == gmids[2] then
     loadstring(game:HttpGet("https://pastefy.app/zo2zurRI/raw"))() 
     print("Sleepy Brainrots loaded")
-elseif id == gmids[3] then
+elseif id == gmids[3] or gmids[4] then
     loadstring(game:HttpGet("https://pastefy.app/aAGy7Acm/raw"))()
     print("Weird gun game loaded")
 else
